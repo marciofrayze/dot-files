1797d696bcd415efa4aa86735e854677bf5b41ec
Marcio Frayze David <mfdavid@gmail.com>
2019-01-31 13:48:47 -0200

    Atualizando configuracoes do xmonad

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,7 +1,20 @@
 import XMonad
 import XMonad.Config.Desktop
+import qualified XMonad.StackSet as W
+import XMonad.Util.EZConfig
 
-main = xmonad desktopConfig
-	{ terminal = "terminator"
-	, borderWidth = 3 
-	}
+myModMask = mod4Mask
+
+main = xmonad $ desktopConfig
+  { modMask = mod4Mask
+  , terminal = "terminator"
+  , borderWidth = 2
+  } `additionalKeysP` myKeys
+ 
+myKeys = [ ("M-C-f", spawn "firefox")
+    , ("M-C-e", spawn "emacs")
+    , ("M-C-c", spawn "code")
+    , ("M-C-i", spawn "idea")
+    , ("M-C-l", spawn "slock")
+    , ("M-C-p", spawn "xfce4-popup-clipman")
+    ]
