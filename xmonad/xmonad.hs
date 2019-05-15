43abd8fcd4feda9b84c0751dc30126bdc6c6d8da
Marcio Frayze David <mfdavid@gmail.com>
2019-05-15 10:14:03 -0300

    Alterando como xmonad trabalha com 2+ monitores

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -10,11 +10,18 @@ main = xmonad $ desktopConfig
   , terminal = "terminator"
   , borderWidth = 2
   } `additionalKeysP` myKeys
- 
+  
+myWorkspaces = ["1","2","3","4","5","6","7","8","9"]
+  
 myKeys = [ ("M-C-f", spawn "firefox")
     , ("M-C-e", spawn "emacs")
     , ("M-C-c", spawn "code")
     , ("M-C-i", spawn "idea")
     , ("M-C-l", spawn "slock")
-    , ("M-C-p", spawn "xfce4-popup-clipman")
+    , ("M-C-p", spawn "gpaste-client ui")
+    ] ++
+    [ (otherModMasks ++ "M-" ++ [key], action tag)
+      | (tag, key)  <- zip myWorkspaces "123456789"
+      , (otherModMasks, action) <- [ ("", windows . W.view)
+                                      , ("S-", windows . W.shift)]
     ]
