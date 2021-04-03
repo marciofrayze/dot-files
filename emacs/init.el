defc0dc4f4cc8cde3802d6a50fe1473dab834540
Marcio Frayze David <marcio.david@serpro.gov.br>
2021-04-03 12:46:07 -0300

    varias melhorias e novos plugins

--- emacs/init.el
+++ emacs/init.el
@@ -17,6 +17,8 @@
   (package-install 'undo-fu))
 (unless (package-installed-p 'magit)               ;; git client
   (package-install 'magit))
+(unless (package-installed-p 'git-gutter)          ;; mark changed/uncommited lines  
+  (package-install 'git-gutter))
 (unless (package-installed-p 'evil-collection)     ;; without this, magit does not play well with evil mode
   (package-install 'evil-collection))
 (unless (package-installed-p 'elfeed)              ;; rss client
@@ -29,6 +31,24 @@
   (package-install 'which-key))
 (unless (package-installed-p 'editorconfig)        ;; obey .editorconfig configurations
   (package-install 'editorconfig))
+(unless (package-installed-p 'yasnippet-snippets)  ;; template / snippets
+  (package-install 'yasnippet-snippets))
+(unless (package-installed-p 'yaml-mode)           ;; yaml support
+  (package-install 'yaml-mode))
+(unless (package-installed-p 'clojure-mode)        ;; clojure support
+  (package-install 'clojure-mode))
+(unless (package-installed-p 'cider)               ;; better clojure support
+  (package-install 'cider))
+(unless (package-installed-p 'aggressive-indent)   ;; better clojure support
+  (package-install 'aggressive-indent))
+(unless (package-installed-p 'rainbow-delimiters)  ;; better clojure support
+  (package-install 'rainbow-delimiters))
+(unless (package-installed-p 'buffer-move)         ;; better clojure support
+  (package-install 'buffer-move))
+(unless (package-installed-p 'org-roam)            ;; slip box like features into org mode
+  (package-install 'org-roam))
+(unless (package-installed-p 'twittering-mode)     ;; twitter support
+  (package-install 'twittering-mode))
 ;; Autocomplete and project management related
 (unless (package-installed-p 'projectile)
   (package-install 'projectile))
@@ -36,16 +56,28 @@
   (package-install 'lsp-mode))
 (unless (package-installed-p 'lsp-ui)
   (package-install 'lsp-ui))
-(unless (package-installed-p 'flycheck)        ;; sintax checking
-  (package-install 'flycheck))
-(unless (package-installed-p 'company)         ;; auto completion
+;;(unless (package-installed-p 'flycheck)            ;; sintax checking
+;;  (package-install 'flycheck))
+;;(unless (package-installed-p 'flycheck-haskell)    ;; sintax checking on the fly for haskell
+;;  (package-install 'flycheck-haskell))
+(unless (package-installed-p 'company)             ;; auto completion
   (package-install 'company))
+(unless (package-installed-p 'company-lsp)         ;; auto completion (lsp integration)
+  (package-install 'company-lsp))
+(unless (package-installed-p 'company-ghci)        ;; auto completion haskell (lsp integration)
+  (package-install 'company-ghci))
 (unless (package-installed-p 'helm-lsp)
   (package-install 'helm-lsp))
-(unless (package-installed-p 'helm-ag)         ;; search using ag
+(unless (package-installed-p 'helm-ag)             ;; search using ag
   (package-install 'helm-ag))
-(unless (package-installed-p 'ivy)             ;; search for a file (better projectile search)
+(unless (package-installed-p 'helm-projectile)     ;; helm projectile integration (better global grep search)
+  (package-install 'helm-projectile))
+(unless (package-installed-p 'ivy)                 ;; search for a file (better projectile search)
   (package-install 'ivy))
+(unless (package-installed-p 'direnv)              ;; better shell
+  (package-install 'direnv))
+(unless (package-installed-p 'better-defaults)     ;; some defaults (suggestion from clojure docs)
+  (package-install 'better-defaults))
 
 ;; Enable evil and evil collection
 (setq evil-want-keybinding nil)
@@ -62,11 +94,13 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(blink-cursor-mode nil)
- '(custom-enabled-themes (quote (tango-dark)))
+ '(custom-enabled-themes '(tango-dark))
+ '(git-gutter:update-interval 3)
  '(helm-minibuffer-history-key "M-p")
  '(inhibit-startup-screen t)
+ '(markdown-command "/usr/local/bin/pandoc")
  '(menu-bar-mode nil)
- '(package-selected-packages (quote (xah-elisp-mode ## lsp-javacomp magit evil)))
+ '(package-selected-packages '(xah-elisp-mode ## magit evil))
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -81,6 +115,14 @@
 ;; Configuring general lsp support
 (require 'lsp-mode)
 (setq lsp-enable-links nil)
+(setq lsp-enable-file-watchers t)
+(setq lsp-file-watch-threshold 10000)
+(global-set-key (kbd "C-c l a") 'lsp-execute-code-action)
+(global-set-key (kbd "C-c l i") 'lsp-organize-imports)
+(global-set-key (kbd "C-c l r") 'lsp-find-references)
+
+;; Configuring helm
+(global-set-key (kbd "C-c h s") 'helm-projectile-ag)
 
 ;; Configuring lsp Elm
 (add-hook 'elm-mode-hook 'elm-format-on-save-mode)
@@ -89,36 +131,54 @@
 ;; Configuring lsp Java
 (require 'lsp-java)
 (add-hook 'java-mode-hook #'lsp)
-;; (add-hook 'after-save-hook 'editorconfig-format-buffer)
 (add-hook 'java-mode-hook
 	  '(lambda ()
-	     (add-hook 'after-save-hook 'editorconfig-format-buffer)
-	     (add-hook 'after-save-hook 'lsp-organize-imports)))
+	     (add-hook 'after-save-hook 'editorconfig-format-buffer)))
+;; 	     (add-hook 'after-save-hook 'lsp-organize-imports)))
+
+;; Configuring lsp haskell
+(add-hook 'haskell-mode-hook #'lsp)
+(add-hook 'haskell-mode-hook 'direnv-update-environment)
+(add-hook 'haskell-literate-mode-hook #'lsp)
+;; (add-hook 'haskell-mode-hook 'company-mode)
+;; (setq haskell-process-type 'stack-ghci) 
 
 ;; Enbales which-key (https://github.com/justbur/emacs-which-key)
 (which-key-mode)
 
-;; Neotree shortcut
+;; Enable git gutter
+(global-git-gutter-mode t)
+
+;; Neotree config
 (global-set-key (kbd "C-x t") 'neotree-toggle)
+(setq neo-window-width 55)
 
 ;; Show line number
-(global-display-line-numbers-mode)
+;; (global-display-line-numbers-mode)
 
-;; Projectile and ivy setup
+;; Projectile and ivy config
 (require 'projectile)
-(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
 (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
-(projectile-mode +1)
-(ivy-mode 1)
+(projectile-mode t)
+(ivy-mode t)
 (setq ivy-use-virtual-buffers t)
 (setq enable-recursive-minibuffers t)
 
-;; Put backups on /tmp and disable lock files
-(setq backup-directory-alist '((".*" . "/tmp")))
-(setq create-lockfiles nil) 
+;; Put backups in a different directory
+(setq backup-directory-alist `(("." . "~/emacs/.saves")))
+(setq delete-old-versions t
+      kept-new-versions 6
+      kept-old-versions 2
+      version-control t)
+
+;; Do not create lockfiles (#file#)
+(setq create-lockfiles nil)
 
 ;; Code formating
 (editorconfig-mode 1)
+
+;; Enable auto complete
+(add-hook 'after-init-hook 'global-company-mode)
 
 ;; RSS Feeds
 (setq elfeed-feeds
@@ -129,3 +189,29 @@
         "https://feed.infoq.com"
         "https://architectelevator.com/feed.xml"
         "https://segunda.tech/rss.xml"))
+
+;; clojure
+(add-hook 'clojure-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'clojure-mode-hook #'aggressive-indent-mode)
+(add-hook 'clojure-mode-hook #'subword-mode)
+
+;; Parenteses with colors
+(show-paren-mode 1)  
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+
+;; Move window buffers around
+(global-set-key (kbd "C-c C-m C-k") 'buf-move-up)
+(global-set-key (kbd "C-c C-m C-j") 'buf-move-down)
+(global-set-key (kbd "C-c C-m C-h") 'buf-move-left)
+(global-set-key (kbd "C-c C-m C-l") 'buf-move-right)
+
+;; Select window by number
+(define-key evil-normal-state-map ",1" 'select-window-1)
+(define-key evil-normal-state-map ",2" 'select-window-2)
+(define-key evil-normal-state-map ",3" 'select-window-3)
+(define-key evil-normal-state-map ",4" 'select-window-4)
+
+;; org-roam
+(unless (file-exists-p "~/org-roam")
+  (setq org-roam-directory "~/org-roam"))
+(add-hook 'after-init-hook 'org-roam-mode)
